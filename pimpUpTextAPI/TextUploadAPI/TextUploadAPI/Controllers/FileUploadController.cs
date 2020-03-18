4f599d2694c9ac25997f4ebabc2be94320927a7a
jjyoon7 <jay.yoon7@gmail.com>
2020-03-18 15:30:51 +0100

    reset ASP.NET API to add controller to upload the file

---
+++ pimpUpTextAPI/TextUploadAPI/TextUploadAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TextUploadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class FileUploadController : Controller
+    {
+        public static IWebHostEnvironment _environment;
+
+        public FileUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public class TextUPloadAPI
+        {
+            public IFormFile files { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<string> Post(TextUPloadAPI objFile)
+        {
+            try
+            {
+                /**/
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\")
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Upload\\" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "\\Upload\\" + objFile.files.FileName;
+                    }
+                }
+                else
+                {
+                    return "Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+    }
+
+    
+}
--- pimpUpTextAPI/pimpUpTextAPI/Models/TextItem.cs
+++
@@ -1,6 +0,0 @@
-﻿public class TextItem
-{
-    public long Id { get; set; }
-    public string Name { get; set; }
-    public string Text { get; set; }
-}
