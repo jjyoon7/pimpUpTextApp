45047af0b91fd2064b48cef487ecd00dc5e4b39c
jjyoon7 <jay.yoon7@gmail.com>
2020-03-28 14:26:54 +0100

    Add Models folder and also add entityCore packs

---
+++ textApp/textApp/Models/File.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace textApp.Models
+{
+    public class File
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string FilePath { get; set; }
+    }
+
+}
---
+++ textApp/textApp/Models/FileDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace textApp.Models
+{
+    public class FileDBContext : DbContext
+    {
+        public FileDBContext(DbContextOptions<FileDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<File> Files { get; set; }
+    }
+}
